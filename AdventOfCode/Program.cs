60ddc9014b0a5b253e3a34ecaff3ab0298b09457
btschirhart <btschirhart@profdata.com>
2015-12-22 16:17:50 -0600

    Initial commit w/ Day 1 Complete

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GoDay1();
+
+            Console.ReadKey();
+        }
+
+        static void GoDay1()
+        {
+            Console.WriteLine("Day1 - " + Day1.Title + "\n");
+            
+            Console.WriteLine("\tPart One :: " + Day1.CheckSantasFloor());
+            foreach (var moo in Day1.CheckBasementPosition().Where(x => x.Key < 0))
+            {
+                Console.WriteLine("\tPart Two :: " + moo.Value);
+            }
+        }
+
+    }
+}
